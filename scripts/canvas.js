baafb0b317ee7895e88ec1d8f28858b3604c0a55
Albert Yip <albertkaiyip@gmail.com>
2018-02-10 21:00:16 -0800

    refactoring part one

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 class characters {
 
 	var name;
@@ -81,4 +82,57 @@ class student extends characters {
 	}
 
 
+=======
+var renderer = PIXI.autoDetectRenderer(800, 600);
+document.body.appendChild(renderer.view);
+
+var stage = new PIXI.Container();
+
+var background = new PIXI.Sprite.fromImage('../GraphicsRF/Assets/car.png');
+stage.addChild(background);
+
+stage.filters = [new PIXI.filters.VoidFilter()];
+stage.filterArea = new PIXI.Rectangle(0, 0, 800, 600);
+
+var blobArray = [];
+var numBlobs = 10;
+
+for (var i = 0; i < numBlobs; i++) {
+    var blob = new PIXI.Sprite.fromImage('../GraphicsRF/Characters/blue_stand/blue_stand.png');
+
+    blob.anchor.set(0.5);
+    blob.scale.set(0.8 + Math.random() * 0.3);
+
+    blob.position.x = Math.floor(Math.random() * renderer.width);
+    blob.position.y = Math.floor(Math.random() * renderer.height);
+
+    blob.blendMode = Math.random()>0.5? PIXI.BLEND_MODES.OVERLAY:PIXI.BLEND_MODES.HARD_LIGHT;
+    blob.direction = Math.random() * Math.PI * 2;
+    blob.turningSpeed = Math.random() - 0.8;
+    blob.speed = 2 + Math.random() * 2;
+
+    blobArray.push(blob);
+
+    stage.addChild(blob);
+>>>>>>> Stashed changes
 }
+
+var tick = 0;
+
+requestAnimationFrame(anime);
+
+function anime() {
+    for (var i = 0; i < blobArray.length; i++) {
+        var blob = blobArray[i];
+        blob.direction = blob.turningSpeed * 0.01;
+        blob.position.x = Math.sin(blob.direction) * blob.speed;
+        blob.position.y = Math.cos(blob.direction) * blob.speed;
+        blob.rotation = -blob.direction - Math.PI / 2;
+    }
+}
+
+tick += 0.1;
+
+renderer.render(stage);
+
+requestAnimationFrame(anime);
